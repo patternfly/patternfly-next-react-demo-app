f72cbc89bdd89b4b470085db4177d983f3eb6537
Mike Turley <mturley@redhat.com>
2018-03-27 14:19:14 -0400

    Add some alerts for now

--- src/App.js
+++ src/App.js
@@ -6,7 +6,35 @@ class App extends Component {
   render() {
     return (
       <div>
-        TODO: Build an app here...
+        <h1>Work in Progress! TODO: Use the Page layout and vertical nav</h1>
+        <div style={{ paddingBottom: 10 }}>
+          <Alert>
+            I am an Alert with type="
+            error
+            "
+          </Alert>
+        </div>
+        <div style={{ paddingBottom: 10 }}>
+          <Alert type="warning">
+            I am an Alert with type="
+            warning
+            "
+          </Alert>
+        </div>
+        <div style={{ paddingBottom: 10 }}>
+          <Alert type="success">
+            I am an Alert with type="
+            success
+            "
+          </Alert>
+        </div>
+        <div style={{ paddingBottom: 10 }}>
+          <Alert type="info">
+            I am an Alert with type="
+            info
+            "
+          </Alert>
+        </div>
       </div>
     );
   }
